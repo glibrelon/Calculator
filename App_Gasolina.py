608f42dab2de9d9f91cbede74dc03d9a5f28aa77
glibrelon <66662635+glibrelon@users.noreply.github.com>
2021-09-10 14:03:22 -0300

    Update App_Gasolina.py

--- App_Gasolina.py
+++ App_Gasolina.py
@@ -1,13 +1,25 @@
 import tkinter as tk
 from tkinter.constants import BOTTOM, LEFT, RIGHT
+from tkinter import messagebox
+
+def calculate():
+    single_travel_distance = int(Dist.get())
+    n_ride = int(n_viagens.get())
+    total_distance_traveled = single_travel_distance * n_ride
+    car_efficiency = int(km_L.get())
+    gas_price = int(price_gas.get())
+    calculo = (total_distance_traveled / car_efficiency ) * ( 1 / gas_price)
+
+    return messagebox.showinfo('message', f'R$ {calculo:.2f}')
 
 root = tk.Tk()
 
 
-root.title("Cálculo da Gasolina")
+# Title and window format
+root.title( "Cálculo da Gasolina" )
 
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=3)
+root.columnconfigure( 0, weight=1 )
+root.columnconfigure( 1, weight=3 )
 
 window_width = 250
 window_height = 250
@@ -15,44 +27,47 @@ window_height = 250
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
-center_X = int(screen_width / 2 - window_width / 2)
-center_Y = int(screen_height / 2 - window_height / 2)
+center_X = int( screen_width / 2 - window_width / 2 )
+center_Y = int( screen_height / 2 - window_height / 2 )
 
-root.geometry(f'{window_width}x{window_height}+{center_X}+{center_Y}')
-
-
-
-label_Dist = tk.Label(root, text = "Distância em\numa viagem")
-label_Dist.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
-Dist = tk.Entry()
-Dist.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
+root.geometry( f'{window_width}x{window_height}+{center_X}+{center_Y}' )
 
 
 
-n_viagens_label = tk.Label(root, text = "Nº de Viagens")
-n_viagens_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
+# Data entries
+    
+    # Distance of a ride entry
+label_Dist = tk.Label( root, text = "Distância em\numa viagem" )
+label_Dist.grid( column=0, row=0, sticky=tk.W, padx=5, pady=5 )
+Dist =  tk.Entry()
+Dist.grid( column=1, row=0, sticky=tk.W, padx=5, pady=5 )
+
+
+    # Total travels
+n_viagens_label = tk.Label( root, text = "Nº de Viagens" )
+n_viagens_label.grid( column=0, row=1, sticky=tk.W, padx=5, pady=5 )
 n_viagens = tk.Entry()
-n_viagens.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
+n_viagens.grid( column=1, row=1, sticky=tk.W, padx=5, pady=5 )
 
 
-
-label_km_L = tk.Label(root, text = "Km/L")
-label_km_L.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
+    # Car Performance
+label_km_L = tk.Label( root, text = "Km/L" )
+label_km_L.grid( column=0, row=2, sticky=tk.W, padx=5, pady=5 )
 km_L = tk.Entry()
-km_L.grid(column=1, row=2, sticky=tk.W, padx=5, pady=5)
+km_L.grid( column=1, row=2, sticky=tk.W, padx=5, pady=5 )
 
 
-
-label_preco_gas = tk.Label(root, text = "Preço\ncombustível/L")
-label_preco_gas.grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
-preco_gas = tk.Entry()
-preco_gas.grid(column=1, row=3, sticky=tk.W, padx=5, pady=5)
-
-
-botao = tk.Button(root, text = "Calcule")
-botao.grid(column=1, row=5, sticky=tk.W, padx=5, pady=5)
+    # Gas Price per liter
+label_preco_gas = tk.Label( root, text = "Preço\ncombustível/L" )
+label_preco_gas.grid( column=0, row=3, sticky=tk.W, padx=5, pady=5 )
+price_gas = tk.Entry()
+price_gas.grid ( column=1, row=3, sticky=tk.W, padx=5, pady=5 )
 
 
-print("oi")
+# Button
+button = tk.Button( root, text = "Calcule", command = calculate )
+button.grid( column=1, row=5, sticky=tk.W, padx=5, pady=5 )
+
+
 
 root.mainloop()
